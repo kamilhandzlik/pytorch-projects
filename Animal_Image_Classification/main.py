5a08df1a1f52c151bec3ce34f0801815c99324d4
kamol0209 <kamol0209@gmail.com>
2025-08-09 11:33:54 +0200

    added dataloaders anf model to animal_image/main

--- Animal_Image_Classification/main.py
+++ Animal_Image_Classification/main.py
@@ -99,3 +99,45 @@ for row in range(n_rows):
         axarr[row, col].axis("off")
 
 plt.show()  # Comment if you don't want to see the images
+
+
+# 10. DataLoaders
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+
+# 11. Model
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pooling = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear((128 * 16 * 16), 128)
+        self.output = nn.Linear(128, len(data_df["labels"].unique()))
+
+    def forward(self, x):
+        x = self.conv1(x)  # -> Outputs: (32, 128, 128)
+        x = self.pooling(x)  # -> Outputs: (32, 64, 64)
+        x = self.relu(x)
+        x = self.conv2(x)  # -> Outputs: (64, 64, 64)
+        x = self.pooling(x)  # -> Outputs: (64, 32, 32)
+        x = self.relu(x)
+        x = self.conv3(x)  # -> Outputs: (128, 32, 32)
+        x = self.pooling(x)  # -> Outputs: (128, 16, 16)
+        x = self.relu(x)
+        x = self.flatten(x)
+        x = self.linear(x)
+        x = self.output(x)
+
+        return x
+
+
+model = Net().to(device)
