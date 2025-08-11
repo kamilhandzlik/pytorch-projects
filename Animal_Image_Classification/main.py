7408429dd9727fd458fa6d09f5d519ca30f3f77c
kamol0209 <kamol0209@gmail.com>
2025-08-11 13:07:59 +0200

    edited main.py in animal image classification. added trainin loop, testing and viualization of accuracy in leraning

--- Animal_Image_Classification/main.py
+++ Animal_Image_Classification/main.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.optim import Adam, SGD
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
+from torchsummary import summary
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -141,3 +142,125 @@ class Net(nn.Module):
 
 
 model = Net().to(device)
+
+
+# 12. Model Summary
+summary(model, input_size=(3, 128, 128))
+
+
+# 13. Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+# 14. Training Loop
+total_loss_train_plot = []
+total_loss_validation_plot = []
+total_acc_train_plot = []
+total_acc_validation_plot = []
+
+
+for epoch in range(EPOCHS):
+    total_loss_train = 0
+    total_loss_validation = 0
+    total_acc_train = 0
+    total_acc_validation = 0
+
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        train_loss = criterion(outputs, labels)
+        total_loss_train += train_loss.item()
+        train_loss.backward()
+        train_acc = (torch.argmax(outputs, dim=1) == labels).sum().item()
+        total_acc_train += train_acc
+        optimizer.step()
+
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            outputs = model(inputs)
+            val_loss = criterion(outputs, labels)
+            total_loss_validation += val_loss.item()
+            val_acc = (torch.argmax(outputs, axis=1) == labels).sum().item()
+            total_acc_validation += val_acc
+
+    total_loss_train_plot.append(round(total_loss_train / 1000, 4))
+    total_loss_validation_plot.append(round(total_loss_validation / 1000, 4))
+    total_acc_train_plot.append(
+        round(total_acc_train / (train_dataset.__len__()) * 100, 4)
+    )
+    total_acc_validation_plot.append(
+        round(total_acc_validation / (val_dataset.__len__()) * 100, 4)
+    )
+    print(
+        f"""Epoch {epoch+1}/{EPOCHS}, Train Loss: {round(total_loss_train/100, 4)} Train Accuracy {round((total_acc_train)/train_dataset.__len__() * 100, 4)}
+              Validation Loss: {round(total_loss_validation/100, 4)} Validation Accuracy: {round((total_acc_validation)/val_dataset.__len__() * 100, 4)}"""
+    )
+    print("=" * 25)
+
+
+# 15. Testing
+with torch.no_grad():
+    total_loss_test = 0
+    total_acc_test = 0
+    for inputs, labels in test_loader:
+        predictions = model(inputs)
+
+        acc = (torch.argmax(predictions, axis=1) == labels).sum().item()
+        total_acc_test += acc
+        test_loss = criterion(predictions, labels)
+        total_loss_test += test_loss.item()
+    print(
+        f"Accuracy Score is: {round((total_acc_test/test_dataset.__len__()) * 100, 4)} and Loss is {round(total_loss_test/1000, 4)}"
+    )
+
+
+# 16. Plotting Training Progress
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+
+axs[0].plot(total_loss_train_plot, label="Training Loss")
+axs[0].plot(total_loss_validation_plot, label="Validation Loss")
+axs[0].set_title("Training and Validation Loss over Epochs")
+axs[0].set_xlabel("Epochs")
+axs[0].set_ylabel("Loss")
+axs[0].legend()
+
+axs[1].plot(total_acc_train_plot, label="Training Accuracy")
+axs[1].plot(total_acc_validation_plot, label="Validation Accuracy")
+axs[1].set_title("Training and Validation Accuracy over Epochs")
+axs[1].set_xlabel("Epochs")
+axs[1].set_ylabel("Accuracy")
+axs[1].legend()
+
+plt.tight_layout()
+
+plt.show()
+
+
+# 17. Inference
+
+# 1- read image
+# 2- Transform using transform object
+# 3- predict through the model
+# 4- inverse transform by label encoder
+
+
+def predict_image(image_path):
+    image = Image.open(image_path).convert("RGB")
+    image = transform(image).to(device)
+
+    output = model(image.unsqueeze(0))
+    output = torch.argmax(output, axis=1).item()
+    return label_encoder.inverse_transform([output])
+
+
+## Visualize the image
+image = Image.open("/content/cute-photos-of-cats-looking-at-camera-1593184780.jpg")
+plt.imshow(image)
+plt.show()
+
+
+## Predict
+print()
+print("Prediction: \n")
+predict_image("/content/cute-photos-of-cats-looking-at-camera-1593184780.jpg")
