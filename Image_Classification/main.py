b7dcd860d6870712b6cdf73c483b231a10f09c77
kamol0209 <kamol0209@gmail.com>
2025-08-22 16:08:45 +0200

    Initial commit for my new ML model

---
+++ Image_Classification/main.py
@@ -0,0 +1,9 @@
+# import kagglehub
+
+# Download latest version
+# path = kagglehub.dataset_download("marquis03/bean-leaf-lesions-classification")
+
+# print("Path to dataset files:", path)
+
+
+# You can download manually from https://www.kaggle.com/datasets/marquis03/bean-leaf-lesions-classification
