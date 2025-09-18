c587359163379404f79864e8502e30dc3d82459e
kamol0209 <kamol0209@gmail.com>
2025-09-18 17:34:28 +0200

    Initial commit begginig of new project

---
+++ Sound_Classification/main.py
@@ -0,0 +1,16 @@
+"""
+Dataset used for training this model is from:
+https://www.kaggle.com/datasets/abdulvahap/music-instrunment-sounds-for-classification?utm_source=chatgpt.com
+
+can be downloaded using this code
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("abdulvahap/music-instrunment-sounds-for-classification")
+
+print("Path to dataset files:", path)
+
+or manually from link above it consist of 3 sec instrumental audioclips.
+
+Ps. You have to have an account on kaggle to donwload this data.
+"""
