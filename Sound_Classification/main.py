ec627820a22f46dbe4eb2e8dd96bf40b448e1eaa
kamol0209 <kamol0209@gmail.com>
2025-09-19 16:45:14 +0200

    changed main.py in sound classification

--- Sound_Classification/main.py
+++ Sound_Classification/main.py
@@ -21,6 +21,8 @@ import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, Dataset
+import torchaudio
+import torchaudio.transforms as T
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -39,6 +41,8 @@ print(
     if device == "cuda"
     else f"\n\033[34mDevice is set to: {device}\033[0m\n"
 )
+# torchaudio.set_audio_backend("soundfile")
+# print(f"torchaudio: {torchaudio}")
 
 
 # 3. Hyperparameters
@@ -66,3 +70,50 @@ for i in os.listdir("Sound_Classification/dataset/"):
 
 data_df = pd.DataFrame(zip(sound_path, labels), columns=["sound_path", "labels"])
 print(data_df.head())
+
+
+# 5. Data split
+train = data_df.sample(frac=0.7, random_state=RANDOM_SEED)
+temp = data_df.drop(train.index)
+val = temp.sample(frac=0.5, random_state=RANDOM_SEED)
+test = temp.drop(val.index)
+
+print(f"train: {train}")
+print(f"test: {temp}")
+print(f"val: {val}")
+print(f"test_val: {test}")
+
+
+# 6. Preprocessing Objects
+label_encoder = LabelEncoder()
+label_encoder.fit(data_df["labels"])
+
+# Mel-spectogram
+transform = nn.Sequential(
+    T.MelSpectrogram(sample_rate=44100, n_fft=1024, hop_length=128), T.AmplitudeToDB()
+)
+
+
+# 7. Custom Dataset Class
+class InstrumentDataset(Dataset):
+    def __init__(self, df, label_encoder, transform=None):
+        self.df = df.reset_index(drop=True)
+        self.label_encoder = label_encoder
+        transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        sound_path = self.df.loc[idx, "sound_path"]
+        label = self.df.loc[idx, "label"]
+
+        waveform, sample_rate = torchaudio.load(sound_path)
+        label = self.label_encoder.transform([label])[0]
+
+        if self.transform:
+            features = self.transform(waveform)
+        else:
+            features = waveform
+
+        return features, label
